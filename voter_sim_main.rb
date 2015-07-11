2db4df9b1d7d4628aedfdd8cb3ee8e8cc05c97eb
Alec Greenberg <aleckyler@comcast.com>
2015-07-11 11:02:23 -0400

    added comments to main menu file

--- voter_sim_main.rb
+++ voter_sim_main.rb
@@ -29,38 +29,36 @@ Are you sure you want to perform this action?
   (B)No
   END
 end
-# def check_response(question, valid_responses, choice)
-#   until valid_responses.include? choice
-#     puts "That is not a valid entry."
-#     question
-#     choice = gets.chomp.downcase
-#   end
-# end
 
 candidates = []
 voters = []
 
-while true
+while true # to loop through entire program if user chooses to continue at end
+
+  # puts main menu and makes sure answer is valid
   main_menu
   choice = gets.chomp.downcase
-  # valid_responses = ["a", "b", "c", "d"]
-  # check_response(main_menu, valid_responses, choice)
-  until (choice == "b" || choice == "a" || choice == "c" || choice == "d")
+  until (choice == "a" || choice == "b" || choice == "c" || choice == "d")
     puts "That is not a valid entry."
     main_menu
     choice = gets.chomp.downcase
   end
 
+  # if user hasn't entered a candidate or voter, they shouldnt try to list, update, or vote
   if !(choice == "a") && voters == [] && candidates == []
       puts "You should probably add a candidate or a voter first..."
+
+  # if user hasnt included at least one candidate and at least one voter, they cant run a vote
   elsif choice == "d" && candidates == []
       puts "You should probably add a candidate first..."
   elsif choice == "d" && voters == []
       puts "You should probably add a voter first..."
 
+  # actions based on valid calling of a main menu item
   else
     case choice
     when "a"
+    # create a new person and add them to "candidates" list or "voters" list
       require './create.rb'
       new_person = create
       p new_person
@@ -75,8 +73,9 @@ while true
         puts voters # to check that list is updating
       end
 
-
     when "c"
+    # run the update function with candidates list and voters list as arguments,
+    # should return both lists back separately
       require './update.rb'
       (candidates,voters)=update(candidates,voters)
     when "b"
@@ -96,6 +95,7 @@ while true
     end
   end
 
+  # check if user wants to continue with current lists or exit program
   to_continue
   continue = gets.chomp.downcase
   until (continue == "a" || continue == "b")
